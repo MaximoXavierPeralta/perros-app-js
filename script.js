8da442de94b4f69651380a32e011843cdc8b9401
MaximoXavierPeralta <maximo.peralta@alu.inspt.utn.edu.ar>
2026-07-14 01:25:24 -0300

    clase-01

--- script.js
+++ script.js
@@ -22,8 +22,8 @@ boton.addEventListener("click", () => {
 
     estado.fotos = [
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7kVwY6E3UkmCgzAWgtGomGiRCMrGr8ZeYqsWeQUCCgEcPTwYvWzjBTEZljwOScmdW0PuSYcVD5CWmP8WEY5MS_0g9ISItFLhQNXgDMx9XSw&s=10",
-        "https://images.dog.ceo/breeds/labrador/n02099712_100.jpg",
-        "https://images.dog.ceo/breeds/pug/n02110958_100.jpg"
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqdne3UjrWnimqr5ZFRBk48RZTlM33gOSeA8SCPiiN4g&s=10",
+        "https://www.lanacion.com.ar/resizer/v2/el-american-pitbull-terrier-es-equilibrado-CIIZKVMQ4JGHJBLCWAK4PY4XIA.jpg?auth=15967ac158f1b3e1076f7a45e2a07a24602e230834a376e2a7d37f1702d8ab05&width=420&height=686&quality=70&smart=true"
     ];
 
     render();
